5bdd75248589394425be095d36a425a250c1f23b
sue445 <sue445@sue445.net>
2016-05-02 21:07:22 +0900

    Remove needless param

--- spec/service_spec.rb
+++ spec/service_spec.rb
@@ -7,12 +7,12 @@ describe file("/etc/sysconfig/consul") do
   it { should exist }
   it { should be_file }
   its(:content) { should match /^GOMAXPROCS=#{node[:consul][:gomaxprocs]}$/ }
+  its(:content) { should match /^OPTIONS=#{node[:consul][:options]}$/ }
 end
 
 describe file("/etc/systemd/system/consul.service"), if: node[:platform] == "redhat" && node[:platform_version].to_i >= 7 do
   it { should exist }
   it { should be_file }
-  its(:content) { should match /^Environment=GOMAXPROCS=#{node[:consul][:gomaxprocs]}$/ }
   its(:content) { should include "ExecStart=#{node[:consul][:bin_dir]}/consul" }
   its(:content) { should include "-data-dir=#{node[:consul][:data_dir]}" }
 end
