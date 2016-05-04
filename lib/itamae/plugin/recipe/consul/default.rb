4d2109b1ca96c2c511d4cb091c8ac7842bf8b3f4
sue445 <sue445@sue445.net>
2016-05-04 15:24:32 +0900

    Refactor: move to setup

--- lib/itamae/plugin/recipe/consul/default.rb
+++ lib/itamae/plugin/recipe/consul/default.rb
@@ -1,19 +1,3 @@
-node.validate! do
-  {
-    consul: {
-      version: string,
-    },
-  }
-end
-
-node.reverse_merge!(
-  consul: {
-    src_dir:    "/usr/local/src",
-    bin_dir:    "/usr/local/bin",
-    data_dir:   "/tmp/consul",
-    gomaxprocs: 2,
-  },
-)
-
+include_recipe "./setup"
 include_recipe "./download"
 include_recipe "./service"
