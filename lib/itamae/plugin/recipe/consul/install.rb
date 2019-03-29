34d32032f74e6496f79ebede48c7bea336c74ae0
nakamura_subaru <subaru.nakamura@drecom.co.jp>
2019-03-29 19:42:22 +0900

    Added unzip option to overwrite consul binary

--- lib/itamae/plugin/recipe/consul/install.rb
+++ lib/itamae/plugin/recipe/consul/install.rb
@@ -7,6 +7,6 @@ http_request "#{node[:consul][:src_dir]}/#{zip_name}" do
   url "https://releases.hashicorp.com/consul/#{node[:consul][:version]}/#{zip_name}"
 end
 
-execute "unzip #{node[:consul][:src_dir]}/#{zip_name} -d #{node[:consul][:bin_dir]}" do
+execute "unzip -o #{node[:consul][:src_dir]}/#{zip_name} -d #{node[:consul][:bin_dir]}" do
   not_if "#{node[:consul][:bin_dir]}/consul --version | grep #{node[:consul][:version]}"
 end
